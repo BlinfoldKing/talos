52c53c7aa19a2b32397c95ef643990848253c088
Ganesha Danu <ganeshadanu1@gmail.com>
2019-12-25 21:23:59 +0700

    add post migration

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -1,7 +1,27 @@
 table! {
+    posts (id) {
+        id -> Uuid,
+        slug -> Varchar,
+        title -> Varchar,
+        content -> Text,
+        banner -> Varchar,
+        author_id -> Uuid,
+        is_draft -> Bool,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        deleted_at -> Nullable<Timestamp>,
+    }
+}
+
+table! {
     users (id) {
         id -> Uuid,
         username -> Varchar,
         password_hash -> Text,
     }
 }
+
+allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
